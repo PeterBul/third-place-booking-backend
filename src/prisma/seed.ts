204b4f8f9e2d819320e121aa4f7734ddf02c5489
PeterBul <pc.bulukin@gmail.com>
2024-04-02 12:21:30 +0200

    Seed with admin user

--- src/prisma/seed.ts
+++ src/prisma/seed.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
+import * as argon from 'argon2';
+
 async function main() {
   const admin = await prisma.role.upsert({
     where: { id: 5150 },
@@ -27,6 +29,44 @@ async function main() {
       name: 'Member',
     },
   });
+
+  const adminUser = await prisma.user.findFirst({
+    where: {
+      email: process.env.ADMIN_EMAIL,
+    },
+  });
+
+  if (!adminUser) {
+    await prisma.user.create({
+      data: {
+        email: process.env.ADMIN_EMAIL,
+        hash: await argon.hash(process.env.ADMIN_PASS),
+        isMemberBloom: false,
+        isMemberThirdPlace: false,
+        firstName: process.env.ADMIN_FIRST_NAME,
+        lastName: process.env.ADMIN_LAST_NAME,
+        confirmed: true,
+        roles: {
+          connect: {
+            id: admin.id,
+          },
+        },
+      },
+    });
+    await prisma.user.update({
+      where: {
+        email: process.env.ADMIN_EMAIL,
+      },
+      data: {
+        roles: {
+          connect: {
+            id: user.id,
+          },
+        },
+      },
+    });
+  }
+
   console.log({
     admin,
     user,
